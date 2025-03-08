ec0106fb5630aa222b2aa5257dfe75f31c0c7619
Jaseemuddin Naseem <110375211+jaseemuddinn@users.noreply.github.com>
2025-03-08 21:39:44 +0530

    Update Cognition.jsx

--- src/components/Cognition.jsx
+++ src/components/Cognition.jsx
@@ -62,12 +62,12 @@ export default function Cognition() {
         <div className="flex space-x-4">
           <a
             className="mt-6 inline-block bg-white text-yellow-600 hover:bg-gradient-to-r hover:from-yellow-500 hover:to-yellow-800 hover:text-white px-5 py-3 rounded-lg text-sm md:text-lg font-bold transition-colors duration-500 hover:shadow-lg"
-            href="/cognition"
+            href="https://forms.gle/zTYsrLw2VwotBF367"
           >
             Call for Registration <br />
-            <span className="text-sm text-gray-600 text-center hover:text-black">
+{/*             <span className="text-sm text-gray-600 text-center hover:text-black">
               (COMING SOON)
-            </span>
+            </span> */}
           </a>
 
           <a
